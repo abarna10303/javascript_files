3c4803f9d4327f3dc26babaeecf64f71c5ae9cbf
abarna10303 <abarnas361@gmail.com>
2022-12-13 16:35:11 +0530

    Conditions

---
+++ conditions/main.js
@@ -0,0 +1,13 @@
+let num=prompt("Enter the Number");
+if(num>0)
+{
+    console.log(num+ " is a Positive Number");
+}
+else if(num<0)
+{
+    console.log(num+ " is a Nagative Number");
+}
+else
+{
+    console.log(num+ " is a This number");
+}
